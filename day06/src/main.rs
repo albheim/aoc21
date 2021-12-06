835ec7079ab8a4cc500eca7a30b357b1f88ba3e4
Albin Heimerson <albin.heimerson@control.lth.se>
2021-12-06 09:20:35 +0100

    day6

---
+++ day06/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs;
+use std::env;
+
+fn run(input: &mut Vec<usize>, steps: usize) {
+    let mut ptr = 0;
+    for i in 0..steps {
+        input[(ptr + 7) % 9] += input[ptr]; 
+        ptr = (ptr + 1) % 9;
+    }
+    println!("{}", input.iter().sum::<usize>())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let version = &args[1];
+
+    let mut fish: Vec<usize> = vec![0; 9];
+
+    for num in fs::read_to_string("input.txt").expect("Could not read file").split(',') {
+        let days = num.parse::<usize>().unwrap();
+        fish[days] += 1;
+    }
+
+    match version.as_str() {
+        "a" => run(&mut fish, 80),
+        "b" => run(&mut fish, 256),
+        _ => panic!("Invalid input")
+    }
+}
