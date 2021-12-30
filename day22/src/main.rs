834b3a114402368d9daf3d546d585672015cc548
Albin Heimerson <albin.heimerson@control.lth.se>
2021-12-30 12:50:12 +0100

    day 22 and 23 stash

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,95 +1,112 @@
 use std::env;
 use std::fs;
-use std::collections::HashMap;
 use std::cmp;
 
-fn run_a(commands: Vec<(bool, Vec<[i128; 2]>)>) {
-    let mut grid: HashMap<(i128, i128, i128), bool> = HashMap::new();
-    for (switch, range) in commands {
-        for x in cmp::max(-50, range[0][0])..(cmp::min(50, range[0][1]) + 1) {
-            for y in cmp::max(-50, range[1][0])..(cmp::min(50, range[1][1]) + 1) {
-                for z in cmp::max(-50, range[2][0])..(cmp::min(50, range[2][1]) + 1) {
-                    *grid.entry((x, y, z)).or_insert(false) = switch;
+fn overlap(v1: [i128; 6], v2: [i128; 6]) -> bool {
+    let xmin = cmp::max(v1[0], v2[0]);
+    let xmax = cmp::min(v1[1], v2[1]);
+    let ymin = cmp::max(v1[2], v2[2]);
+    let ymax = cmp::min(v1[3], v2[3]);
+    let zmin = cmp::max(v1[4], v2[4]);
+    let zmax = cmp::min(v1[5], v2[5]);
+    return xmax > xmin && ymax > ymin && zmax > zmin;
+}
+
+// Maybe use, but messier code
+fn inside(v1: [i128; 6], v2: [i128; 6]) -> bool {
+    v1[0] > v2[0] && v1[1] < v2[1] && 
+    v1[2] > v2[2] && v1[3] < v2[3] && 
+    v1[4] > v2[4] && v1[5] < v2[5] 
+}
+
+fn run(commands: Vec<(bool, [i128; 6])>) {
+    let mut volumes = Vec::new();
+    for (flag, volume) in commands {
+        println!("volume {:?} on {}", volume, flag);
+        let mut i = 0;
+        while i < volumes.len() {
+            //println!("i = {}, vollen = {}", i, volumes.len());
+            if overlap(volume, volumes[i]) {
+                println!("  overlap with {:?}", volumes[i]);
+                let vnew = volume;
+                let vold = volumes[i];
+                // Modify old piece to not overlap, add new piece at end of volumes
+                // Cut in x
+                if vnew[0] > vold[0] && vnew[0] < vold[1] {
+                    println!("  cut1 at x={}", vnew[0]);
+                    // Cut vold at vnew[0]
+                    volumes[i] = [vold[0], vnew[0] - 1, vold[2], vold[3], vold[4], vold[5]];
+                    volumes.push([vnew[0], vold[1], vold[2], vold[3], vold[4], vold[5]]);
+                } else if vnew[1] > vold[0] && vnew[1] < vold[1] {
+                    println!("  cut2 at x={}", vnew[1]);
+                    // Cut vold at vnew[1]
+                    volumes[i] = [vnew[1] + 1, vold[1], vold[2], vold[3], vold[4], vold[5]];
+                    volumes.push([vold[0], vnew[1], vold[2], vold[3], vold[4], vold[5]]);
+                } else if vnew[2] > vold[2] && vnew[2] < vold[3] {
+                    println!("  cut1 at y={}", vnew[2]);
+                    // Cut vold at vnew[2]
+                    volumes[i] = [vold[0], vold[1], vold[2], vnew[2] - 1, vold[4], vold[5]];
+                    volumes.push([vold[0], vold[1], vnew[2], vold[3], vold[4], vold[5]]);
+                } else if vnew[3] > vold[2] && vnew[3] < vold[3] {
+                    println!("  cut2 at y={}", vnew[3]);
+                    // Cut vold at vnew[3]
+                    volumes[i] = [vold[0], vold[1], vnew[3] + 1, vold[3], vold[4], vold[5]];
+                    volumes.push([vold[0], vold[1], vold[2], vnew[3], vold[4], vold[5]]);
+                } else if vnew[4] > vold[4] && vnew[4] < vold[5] {
+                    println!("  cut1 at z={}", vnew[4]);
+                    // Cut vold at vnew[4]
+                    volumes[i] = [vold[0], vold[1], vold[2], vold[3], vold[4], vnew[4] - 1];
+                    volumes.push([vold[0], vold[1], vold[2], vold[3], vnew[4], vold[5]]);
+                } else if vnew[5] > vold[4] && vnew[5] < vold[5] {
+                    println!("  cut2 at z={}", vnew[5]);
+                    // Cut vold at vnew[3]
+                    volumes[i] = [vold[0], vold[1], vold[2], vold[3], vnew[5] + 1, vold[5]];
+                    volumes.push([vold[0], vold[1], vold[2], vnew[3], vold[4], vnew[5]]);
+                } else {
+                    // If overlap, but no cut, volume[i] is contained in volume and can thus be removed
+                    volumes.remove(i); 
+                    println!("  no more cut");
                 }
             }
+            i += 1;
         }
-    }
-    grid.retain(|_, v| *v);
-    println!("{}", grid.len());
-}
-
-fn volume(range: &Vec<[i128; 2]>) -> i128 {
-    range.iter().map(|range| range[1] - range[0]).product()
-}
-
-fn overlap(rangea: &Vec<[i128; 2]>, rangeb: &Vec<[i128; 2]>) -> i128 {
-    println!("{:?} {:?}", rangea, rangeb);
-    let xmin = cmp::max(rangea[0][0], rangeb[0][0]);
-    let xmax = cmp::min(rangea[0][1], rangeb[0][1]);
-    let ymin = cmp::max(rangea[1][0], rangeb[1][0]);
-    let ymax = cmp::min(rangea[1][1], rangeb[1][1]);
-    let zmin = cmp::max(rangea[2][0], rangeb[2][0]);
-    let zmax = cmp::min(rangea[2][1], rangeb[2][1]);
-    let dx = cmp::max(0, xmax - xmin);
-    let dy = cmp::max(0, ymax - ymin);
-    let dz = cmp::max(0, zmax - zmin);
-    return dx * dy * dz;
-}
-
-fn run_b(commands: Vec<(bool, Vec<[i128; 2]>)>) {
-    let mut count: i128 = 0;
-    for i in 0..commands.len() {
-        let (switch, range) = &commands[i];
-        if *switch {
-            count += volume(range);
-            println!("adding {}", volume(range));
+        // If turned on add whole new volume, all overlaps has been removed from already existing
+        if flag {
+            volumes.push(volume);
         }
-        // If switch
-        for j in 0..i {
-            let (switch_old, range_old) = &commands[j];
-            let overlap = overlap(range, range_old);
-            println!("overlap {}", overlap);
-            if overlap > 0 {
-                if *switch {
-                    if *switch_old {
-                        count -= overlap;
-                        println!("removing");
-                    } else {
-                        count += overlap;
-                        println!("adding");
-                    }
-                } else if *switch_old {
-                    count -= overlap;
-                    println!("removing");
-                }
-            }
-        }
+        let vol: i128 = volumes.iter().map(|v| (v[1] - v[0] + 1) * (v[3] - v[2] + 1) * (v[5] - v[4] + 1)).sum();
+        println!("current volume {}", vol);
+        println!("volumes {:?}", volumes);
     }
 
-    println!("{}", count);
+    let vol: i128 = volumes.iter().map(|v| (v[1] - v[0] + 1) * (v[3] - v[2] + 1) * (v[5] - v[4] + 1)).sum();
+    println!("{}", vol);
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let version = &args[1];
 
-    let commands = fs::read_to_string("test.txt")
+    let commands = fs::read_to_string("test2.txt")
         .expect("Could not read file")
         .lines()
         .map(|line| {
             let (switch, region) = line.split_once(" ").unwrap();
             let switch = switch == "on";
-            let region = region.split(',')
+            let region: Vec<[i128; 2]> = region.split(',')
                 .map(|coord| {
                     let (a, b) = coord[2..].split_once("..").unwrap();
                     [a.parse().unwrap(), b.parse().unwrap()]
                 }).collect();
-            (switch, region)
+            (switch, [region[0][0], region[0][1], region[1][0], region[1][1], region[2][0], region[2][1]])
         }).collect();
 
     match version.as_str() {
-        "a" => run_a(commands),
-        "b" => run_b(commands),
+        "a" => {
+            //commands.retain(|(flag, v)| v.iter().all(|c| c.abs() <= 50));
+            run(commands)
+        },
+        "b" => run(commands),
         _ => panic!("Invalid input")
     }
 }
--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,13 +1,62 @@
 use std::env;
 use std::fs;
 
-fn search(map: &mut Vec<Vec<char>>) {
-    let mincost = std::usize::MAX_VALUE;
-    let types = ['A', 'B', 'C', 'D'];
+fn getx(a: char) -> usize {
+    match a {
+        'A' => 3,
+        'B' => 5,
+        'C' => 7,
+        'D' => 9,
+        _ => panic!("Invalid value")
+    }
+}
 
-    // split in two steps, move out and move in
+fn move_down(map: &Vec<Vec<char>>, pos: [usize; 2]) -> Vec<[usize; 4]> {
+    let mut x = pos[0];
+    let mut y = 1;
 
-    // for each that can move out
+    let xtarg = getx(map[pos[0]][pos[1]]);
+    let step = if x > xtarg { 1 } else { -1 };
+
+    while map[y][x] == '.' {
+        // Precalc would be faster
+        if xtarg < x {
+            x -= 1;
+        } else {
+            x += 1;
+        }
+        if x == xtarg {
+            while map[y + 1][x] == '.' {
+                y += 1;
+            }
+            return Vec::from([[pos[0], pos[1], x, y]]);
+        }
+    }
+    return Vec::new();
+}
+
+fn move_up(map: &Vec<Vec<char>>, pos: [usize; 2]) -> Vec<[usize; 4]> {
+    let mut moves = Vec::new();
+    let mut x = pos[0];
+    while map[1][x] == '.' {
+        if map[2][x] == '#' {
+            moves.push([pos[0], pos[1], x, 1]);
+        }
+        x -= 1;
+    }
+    x = pos[0];
+    while map[1][x] == '.' {
+        if x % 2 == 0 {
+            moves.push([pos[0], pos[1], x, 1]);
+        }
+        x += 1;
+    }
+    moves
+}
+
+/* Start and end coords for move */
+fn possible_moves() -> Vec<[usize; 4]> {
+    let mut moves = Vec::new();
     for i in 0..4 {
         let x = 2 * i + 3;
         let mut correct = true;
@@ -28,11 +77,17 @@ fn search(map: &mut Vec<Vec<char>>) {
     }
 
     // for each that can move in
-    for {
+    for i in {
         // do change
         // recurse
         // undo change
     }
+} 
+
+fn search(map: &mut Vec<Vec<char>>) {
+    let mincost = std::usize::MAX_VALUE;
+    let types = ['A', 'B', 'C', 'D'];
+
     
     if mincost == std::usize::MAX_VALUE {
         mincost = 0;
