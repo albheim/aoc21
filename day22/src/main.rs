554f0a89bdd226cc861bb7a4ea8e93efb2c775bd
Albin Heimerson <albin.heimerson@control.lth.se>
2022-01-01 19:53:29 +0100

    day22 done

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -12,61 +12,45 @@ fn overlap(v1: [i128; 6], v2: [i128; 6]) -> bool {
     return xmax >= xmin && ymax >= ymin && zmax >= zmin;
 }
 
-// Maybe use, but messier code
-fn inside(v1: [i128; 6], v2: [i128; 6]) -> bool {
-    v1[0] > v2[0] && v1[1] < v2[1] && 
-    v1[2] > v2[2] && v1[3] < v2[3] && 
-    v1[4] > v2[4] && v1[5] < v2[5] 
-}
-
 fn run(commands: Vec<(bool, [i128; 6])>) {
     let mut volumes = Vec::new();
     for (flag, volume) in commands {
-        println!("volume {:?} on {}", volume, flag);
         let mut i = 0;
-        let vollen = volumes.len(); // Only loop to curren tlen
-        while i < vollen {
-            println!(" checking = {:?}", volumes[i]);
+        let mut old_len = volumes.len(); // Only loop to curren tlen
+        while i < old_len {
             if overlap(volume, volumes[i]) {
-                println!("  overlap");
                 let mut overlap = volumes[i]; // overlap is copy
                 // Modify old piece to not overlap, add new piece at end of volumes
                 // Cut in x
                 loop {
                     if volume[0] > overlap[0] && volume[0] <= overlap[1] {
-                        println!("  cut1 at x={}", volume[0]);
                         // Cut overlap at volume[0]
                         volumes.push([overlap[0], volume[0] - 1, overlap[2], overlap[3], overlap[4], overlap[5]]);
                         overlap[0] = volume[0];
                     } else if volume[1] >= overlap[0] && volume[1] < overlap[1] {
-                        println!("  cut2 at x={}", volume[1]);
                         // Cut overlap at volume[1]
                         volumes.push([volume[1] + 1, overlap[1], overlap[2], overlap[3], overlap[4], overlap[5]]);
                         overlap[1] = volume[1];
                     } else if volume[2] > overlap[2] && volume[2] <= overlap[3] {
-                        println!("  cut1 at y={}", volume[2]);
                         // Cut overlap at volume[2]
                         volumes.push([overlap[0], overlap[1], overlap[2], volume[2] - 1, overlap[4], overlap[5]]);
                         overlap[2] = volume[2];
                     } else if volume[3] >= overlap[2] && volume[3] < overlap[3] {
-                        println!("  cut2 at y={}", volume[3]);
                         // Cut overlap at volume[3]
                         volumes.push([overlap[0], overlap[1], volume[3] + 1, overlap[3], overlap[4], overlap[5]]);
                         overlap[3] = volume[3];
                     } else if volume[4] > overlap[4] && volume[4] <= overlap[5] {
-                        println!("  cut1 at z={}", volume[4]);
                         // Cut overlap at volume[4]
                         volumes.push([overlap[0], overlap[1], overlap[2], overlap[3], overlap[4], volume[4] - 1]);
                         overlap[4] = volume[4];
                     } else if volume[5] >= overlap[4] && volume[5] < overlap[5] {
-                        println!("  cut2 at z={}", volume[5]);
                         // Cut overlap at volume[3]
                         volumes.push([overlap[0], overlap[1], overlap[2], overlap[3], volume[5] + 1, overlap[5]]);
                         overlap[5] = volume[5];
                     } else {
                         // If overlap, but no cut, volume[i] is contained in volume and can thus be removed
                         volumes.remove(i); 
-                        println!("  no more cut, overlap was {:?}", overlap);
+                        old_len -= 1;
                         break;
                     }
                 }
@@ -78,20 +62,27 @@ fn run(commands: Vec<(bool, [i128; 6])>) {
         if flag {
             volumes.push(volume);
         }
-        let vol: i128 = volumes.iter().map(|v| (v[1] - v[0] + 1) * (v[3] - v[2] + 1) * (v[5] - v[4] + 1)).sum();
-        println!("current volume {}", vol);
-        println!("volumes {:?}", volumes);
     }
 
     let vol: i128 = volumes.iter().map(|v| (v[1] - v[0] + 1) * (v[3] - v[2] + 1) * (v[5] - v[4] + 1)).sum();
     println!("{}", vol);
 }
 
+fn run_a(commands: Vec<(bool, [i128; 6])>) {
+    let mut subcommands = Vec::new();
+    for command in commands {
+        if command.1.iter().all(|c| c.abs() <= 50) {
+            subcommands.push(command);
+        } 
+    }
+    run(subcommands);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let version = &args[1];
 
-    let commands = fs::read_to_string("test2.txt")
+    let commands: Vec<(bool, [i128; 6])> = fs::read_to_string("input.txt")
         .expect("Could not read file")
         .lines()
         .map(|line| {
@@ -106,10 +97,7 @@ fn main() {
         }).collect();
 
     match version.as_str() {
-        "a" => {
-            //commands.retain(|(flag, v)| v.iter().all(|c| c.abs() <= 50));
-            run(commands)
-        },
+        "a" => run_a(commands),
         "b" => run(commands),
         _ => panic!("Invalid input")
     }
