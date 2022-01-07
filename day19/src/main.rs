7ebe1171c99ab0778b59837580bfd37fdb933322
Albin Heimerson <albin.heimerson@control.lth.se>
2022-01-07 15:25:41 +0100

    speedup day19

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,151 +1,185 @@
 use std::env;
 use std::fs;
+use std::collections::HashSet;
 
-fn rotate(p: [isize; 3], rot: usize) -> [isize; 3] {
-    // rotation based on which ends up in positive x direction, then four up configs from that
-    match rot {
-        // x pos
-        0 => [p[0], p[1], p[2]],
-        1 => [p[0], -p[2], p[1]],
-        2 => [p[0], -p[1], -p[2]],
-        3 => [p[0], p[2], -p[1]],
-        // x neg
-        4 => [-p[0], -p[1], p[2]],
-        5 => [-p[0], -p[2], -p[1]],
-        6 => [-p[0], p[1], -p[2]],
-        7 => [-p[0], p[2], p[1]],
-        // y pos
-        8 => [p[1], -p[0], p[2]],
-        9 => [p[1], -p[2], -p[0]],
-        10 => [p[1], p[0], -p[2]],
-        11 => [p[1], p[2], p[0]],
-        // y neg
-        12 => [-p[1], p[0], p[2]],
-        13 => [-p[1], -p[2], p[0]],
-        14 => [-p[1], -p[0], -p[2]],
-        15 => [-p[1], p[2], -p[0]],
-        // z pos
-        16 => [p[2], p[1], -p[0]],
-        17 => [p[2], p[0], p[1]],
-        18 => [p[2], -p[1], p[0]],
-        19 => [p[2], -p[0], -p[1]],
-        // z neg
-        20 => [-p[2], p[1], p[0]],
-        21 => [-p[2], -p[0], p[1]],
-        22 => [-p[2], -p[1], -p[0]],
-        23 => [-p[2], p[0], -p[1]],
-        _ => panic!("invalid rotation")
+#[derive(PartialEq, Eq, Hash, Debug, Default, Clone, Copy)]
+struct Position {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+impl Position {
+    fn subtract(&self, other: &Self) -> Self {
+        Self {
+            x: self.x - other.x, 
+            y: self.y - other.y, 
+            z: self.z - other.z
+        }
+    }
+
+    fn add(&self, other: &Self) -> Self {
+        Self {
+            x: self.x + other.x, 
+            y: self.y + other.y, 
+            z: self.z + other.z
+        }
+    }
+
+    fn dist(&self, other: &Self) -> u64 {
+        let a = self.subtract(other);
+        (a.x.abs() + a.y.abs() + a.z.abs()) as u64
+    }
+
+    fn rotation(&self, rot: u64) -> Self {
+        // rotation based on which ends up in positive x direction, then four up configs from that
+        match rot {
+            // x pos
+            0 => Self { x: self.x, y: self.y, z: self.z},
+            1 => Self { x: self.x, y: -self.z, z: self.y},
+            2 => Self { x: self.x, y: -self.y, z: -self.z},
+            3 => Self { x: self.x, y: self.z, z: -self.y},
+            // x neg
+            4 => Self { x: -self.x, y: -self.y, z: self.z},
+            5 => Self { x: -self.x, y: -self.z, z: -self.y},
+            6 => Self { x: -self.x, y: self.y, z: -self.z},
+            7 => Self { x: -self.x, y: self.z, z: self.y},
+            // y pos
+            8 => Self { x: self.y, y: -self.x, z: self.z},
+            9 => Self { x: self.y, y: -self.z, z: -self.x},
+            10 => Self { x: self.y, y: self.x, z: -self.z},
+            11 => Self { x: self.y, y: self.z, z: self.x},
+            // y neg
+            12 => Self { x: -self.y, y: self.x, z: self.z},
+            13 => Self { x: -self.y, y: -self.z, z: self.x},
+            14 => Self { x: -self.y, y: -self.x, z: -self.z},
+            15 => Self { x: -self.y, y: self.z, z: -self.x},
+            // z pos
+            16 => Self { x: self.z, y: self.y, z: -self.x},
+            17 => Self { x: self.z, y: self.x, z: self.y},
+            18 => Self { x: self.z, y: -self.y, z: self.x},
+            19 => Self { x: self.z, y: -self.x, z: -self.y},
+            // z neg
+            20 => Self { x: -self.z, y: self.y, z: self.x},
+            21 => Self { x: -self.z, y: -self.x, z: self.y},
+            22 => Self { x: -self.z, y: -self.y, z: -self.x},
+            23 => Self { x: -self.z, y: self.x, z: -self.y},
+            _ => panic!("invalid rotation")
+        }
     }
 }
 
-fn diff(a: [isize; 3], b: [isize; 3]) -> [isize; 3] {
-    [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
+struct Scanner {
+    probes: Vec<Position>, // List of probe positions
+    pos: Position,
+    fingerprint: Vec<u64>, // Sorted list of internal distances
 }
 
-fn add(a: [isize; 3], b: [isize; 3]) -> [isize; 3] {
-    [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
-}
+impl Scanner {
+    // If a scanner can match it will have at least 12 + 11 + ... + 1 = 78
+    // internal distances matching eachother.
+    fn can_match(&self, other: &Scanner) -> bool {
+        let mut idx1 = 0; 
+        let mut idx2 = 0;
+        let mut found = 0;
+        let target = 78; // 12 + 11 + ... + 1
 
-fn check_overlap(a: &mut Vec<[isize; 3]>, b: &Vec<[isize; 3]>) -> [isize; 3] {
-    // A is always in base orientation, new probes are added to it with time
-    // Should be possible to shorten this
-    for i in 0..a.len() { //[..a.len()-11] {
-        for j in 0..b.len() {
-            for rot in 0..24 {
-                let mut count = 0;
-                let offset = diff(a[i], rotate(b[j], rot));
-                for l in 0..b.len() {
-                    let b_rot_offset = add(offset, rotate(b[l], rot));
-                    for k in 0..a.len() { //[..a.len()-11] {
-                        if a[k][0] == b_rot_offset[0] && a[k][1] == b_rot_offset[1] && a[k][2] == b_rot_offset[2] {
-                            // println!("at {:?} for rot {} against {:?}", a[k], rot, b[l]);
-                            count += 1;
-                            break;
-                        }
-                    }
-                }
-                // println!("{}", count);
-                if count >= 12 {
-                    // figure out transform and save?
-                    // println!("transform {:?}", offset);
-                    // println!("asdf");
-                    for l in 0..b.len() {
-                        let b_rot_offset = add(offset, rotate(b[l], rot));
-                        let mut found = false;
-                        for k in 0..a.len() { //[..a.len()-11] {
-                            if a[k][0] == b_rot_offset[0] && a[k][1] == b_rot_offset[1] && a[k][2] == b_rot_offset[2] {
-                                found = true;
+        while idx1 < self.fingerprint.len() && idx2 < other.fingerprint.len() && found < target {
+            if self.fingerprint[idx1] < other.fingerprint[idx2] {
+                idx1 += 1;
+            } else if self.fingerprint[idx1] > other.fingerprint[idx2] {
+                idx2 += 1;
+            } else {
+                found += 1;
+                idx1 += 1;
+                idx2 += 1;
+            }
+        }
+
+        return found >= target; 
+    }
+
+    // If it can match, we do the more expensive look to find the actual 
+    // transformation needed.
+    fn find_transform(&self, other: &Scanner) -> (Position, u64) {
+        for i in 0..self.probes.len() { 
+            for j in 0..other.probes.len() {
+                for rot in 0..24 {
+                    let mut count = 0;
+                    let offset = self.probes[i].subtract(&other.probes[j].rotation(rot));
+                    let mut matched = Vec::new();
+                    for l in 0..other.probes.len() {
+                        let updated_probe_position = offset.add(&other.probes[l].rotation(rot));
+                        for k in 0..self.probes.len() { 
+                            if self.probes[k] == updated_probe_position {
+                                matched.push(self.probes[k]);
+                                count += 1;
                                 break;
                             }
                         }
-                        if !found {
-                            a.push(b_rot_offset);
-                        }
                     }
-                    return offset;
+                    if count >= 12 {
+                        return (offset, rot);
+                    }
                 }
             }
         }
+        panic!("Error, should not happen");
     }
-    return [0, 0, 0];
 }
 
-fn run_a(scanners: &mut Vec<Vec<[isize; 3]>>) {
-    let mut base = scanners[0].clone();
-    let mut idxs: Vec<usize> = (1..scanners.len()).collect();
+fn match_scanners(scanners: &mut Vec<Scanner>) {
+    let mut added = vec![false; scanners.len()];
+    added[0] = true;
+
     let mut i = 0;
-    //println!("{}", check_overlap(&mut base, &scanners[1]));
     loop {
-        i %= idxs.len();
-        println!("checking {}", idxs[i]);
-        let scanner_pos = check_overlap(&mut base, &scanners[idxs[i]]);
-        if scanner_pos[0] != 0 || scanner_pos[1] != 0 || scanner_pos[2] != 0 {
-            println!("removed");
-            idxs.remove(i);
-            if idxs.len() == 0 {
+        for j in 0..scanners.len() {
+            if !added[j] && i != j && scanners[i].can_match(&scanners[j]) {
+                added[j] = true;
+                let (offset, rot) = scanners[i].find_transform(&scanners[j]);
+                // Rotate all probes in j to use same coordinates as i
+                for k in 0..scanners[j].probes.len() {
+                    scanners[j].probes[k] = offset.add(&scanners[j].probes[k].rotation(rot));
+                }
+                scanners[j].pos = offset;
+            }
+        }
+        if added.iter().all(|x| *x) {
+            break;
+        }
+        loop {
+            i = (i + 1) % scanners.len();
+            if added[i] { 
                 break;
             }
-        } else {
-            i += 1;
         }
     }
-    println!("{}", base.len());
 }
 
-fn run_b(scanners: &Vec<Vec<[isize; 3]>>) {
-    let mut base = scanners[0].clone();
-    let mut idxs: Vec<usize> = (1..scanners.len()).collect();
-    let mut i = 0;
-    let mut scanner_positions = vec![[0, 0, 0]];
-    //println!("{}", check_overlap(&mut base, &scanners[1]));
-    loop {
-        i %= idxs.len();
-        println!("checking {}", idxs[i]);
-        let scanner_pos = check_overlap(&mut base, &scanners[idxs[i]]);
-        if scanner_pos[0] != 0 || scanner_pos[1] != 0 || scanner_pos[2] != 0 {
-            scanner_positions.push(scanner_pos);
-            println!("removed");
-            idxs.remove(i);
-            if idxs.len() == 0 {
-                break;
-            }
-        } else {
-            i += 1;
+fn run_a(scanners: &mut Vec<Scanner>) {
+    match_scanners(scanners);
+    let mut unique = HashSet::new();
+    for i in 0..scanners.len() {
+        for j in 0..scanners[i].probes.len() {
+            unique.insert(scanners[i].probes[j]);
         }
     }
-    let mut maxlen = 0;
-    for i in 0..scanner_positions.len() {
-        for j in 0..scanner_positions.len() {
-            let dist = (scanner_positions[i][0] - scanner_positions[j][0]).abs() + 
-                (scanner_positions[i][1] - scanner_positions[j][1]).abs() + 
-                (scanner_positions[i][2] - scanner_positions[j][2]).abs();
-            if dist > maxlen {
-                maxlen = dist;
+    println!("{}", unique.len());
+}
+
+fn run_b(scanners: &mut Vec<Scanner>) {
+    match_scanners(scanners);
+    let mut maxdist = 0;
+    for i in 0..scanners.len() {
+        for j in (i + 1)..scanners.len() {
+            let dist = scanners[i].pos.dist(&scanners[j].pos);
+            if dist > maxdist {
+                maxdist = dist;
             }
         }
     }
-    println!("{}", base.len());
-    println!("{}", maxlen);
+    println!("{}", maxdist);
 }
 
 
@@ -153,20 +187,29 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let version = &args[1];
 
-    let mut scanners: Vec<Vec<[isize; 3]>> = fs::read_to_string("input.txt")
+    let mut scanners: Vec<Scanner> = fs::read_to_string("input.txt")
         .expect("Could not read file")
         .split("\n\n")
-        .map(|probe_chunk| probe_chunk.lines().skip(1)
+        .map(|probe_chunk| {
+            let probes: Vec<Position> = probe_chunk.lines().skip(1)
             .map(|line| {
-                let pos: Vec<isize> = line.split(',').map(|num| num.parse().unwrap()).collect();
-                [pos[0], pos[1], pos[2]]
-            }).collect()
+                let pos: Vec<i64> = line.split(',').map(|num| num.parse().unwrap()).collect();
+                Position { x: pos[0], y: pos[1], z: pos[2] }
+            }).collect();
+            let mut fingerprint = Vec::new();
+            for i in 0..probes.len() {
+                for j in 0..i {
+                    fingerprint.push(probes[i].dist(&probes[j]))
+                }
+            }
+            fingerprint.sort();
+            Scanner { probes, pos: Position { x: 0, y: 0, z: 0}, fingerprint }
+        }
         ).collect();
 
     match version.as_str() {
         "a" => run_a(&mut scanners),
         "b" => run_b(&mut scanners),
-        "test" => run_a(&mut vec![vec![[1, 1, 1], [-1, 2, 3]], vec![[-1, -1, -1], [3, 3, 3]]]),
         _ => panic!("Invalid input")
     }
 }
